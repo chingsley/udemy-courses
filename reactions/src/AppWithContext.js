b2544a406627549c340e2c4a9978d5b250884902
chingsley <eneja.kc@gmail.com>
2021-09-29 20:45:55 +0100

    - Use pubnub to publish messages

--- reactions/src/AppWithContext.js
+++ reactions/src/AppWithContext.js
@@ -1,17 +1,35 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import Context from './context';
 import reducer, { initialState } from './state/reducer';
+import PubSub from './pubsub';
 import PublicMessage from './contextComponents/PublicMessage';
 import MessageBoard from './contextComponents/MessageBoard';
 
-import './pubsub';
 import SetUsername from './contextComponents/SetUsername';
+
+const pubsub = new PubSub();
+
+setTimeout(() => {
+  pubsub.publish({ type: 'foo', value: 'bar' });
+}, 1000);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    pubsub.addListener({
+      message: (messageObject) => {
+        const { channel, message } = messageObject;
+
+        console.log('Received message', message, 'channel', channel);
+
+        dispatch(message);
+      },
+    });
+  }, []);
+
   return (
-    <Context.Provider value={{ state, dispatch }}>
+    <Context.Provider value={{ state, dispatch, pubsub }}>
       <h2>Reaction</h2>
       <hr />
       <SetUsername />
--- reactions/src/contextComponents/MessageBoard.js
+++ reactions/src/contextComponents/MessageBoard.js
@@ -4,22 +4,24 @@ import React from 'react';
 // import Context from '../context';
 import { useAppContext } from '../customHooks';
 import CreateReaction from './CreateReaction';
+import MessageReactions from './MessageReactions';
 
 function MessageBoard() {
   const {
-    state: { messages },
+    state: { messages, reactionsMap },
   } = useAppContext();
 
   return (
     <div>
       {messages.map((message) => {
-        const { id, text, timestamp, username } = message;
+        const { text, timestamp, username } = message;
         return (
-          <div key={id}>
+          <div key={message.id}>
             <h4>{new Date(timestamp).toLocaleDateString()}</h4>
             <p>{text}</p>
             <h4>- {username}</h4>
-            <CreateReaction messageId={id} />
+            <CreateReaction messageId={message.id} />
+            <MessageReactions messageReactions={reactionsMap[message.id]} />
             <hr />
           </div>
         );
